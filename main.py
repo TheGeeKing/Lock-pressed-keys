cb2bf5bbc96b5d3f070f868ead38c9e61537592d
TheGeeKing <58857539+TheGeeKing@users.noreply.github.com>
2022-06-27 09:54:50 +0100

    Lock pressed keys v1.2
    Fixed:
    - Python Conventions formatting putting wrong spaces at some places
    - Check if `useful_key` is set
    
    Added:
    - Button color green to start and red to stop

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ import keyboard
 import pyautogui
 import PySimpleGUI as sg
 
+# TODO: remap/catch the key so it doesn't do it's previous action
+
 if getattr(sys, 'frozen', False): # Running as compiled
     os.chdir(sys._MEIPASS) # change current working directory to sys._MEIPASS
 
 sg.theme('DarkAmber')
-layout = [[sg.Text('Set the key to detect:'), sg.Button('Click Me!', key = '-SELECT-')], [sg.Text('Last X keys'), sg.Slider(range = (1, 10), default_value = 1, orientation = 'horizontal', key = '-SLIDER-')], [sg.Button('START', key = '-START-')]]
+layout = [[sg.Text('Set the key to detect:'), sg.Button('Click Me!', key='-SELECT-')], [sg.Text('Last X keys'), sg.Slider(range=(1, 10), default_value=1, orientation='horizontal', key='-SLIDER-')], [sg.Button('START', button_color='green', key='-START-')]]
 
-window = sg.Window('Lock pressed keys', layout, icon = "MMA.ico")
+window = sg.Window('Lock pressed keys', layout, icon='MMA.ico')
 
 class UsefulKey:
     def __init__(self, key):
@@ -31,7 +33,7 @@ class UsefulKey:
                 else:
                     self.do_run = True
                     save_queue = queue.clean_queue()
-                    threading.Thread(target = self.keep_key_pressed, args = (save_queue, )).start()
+                    threading.Thread(target=self.keep_key_pressed, args=(save_queue, )).start()
 
     def keep_key_pressed(self, save_queue: list):
         while self.do_run:
@@ -74,25 +76,25 @@ def register_keys():
 run = False
 
 while True:
-    event, values = window.Read(timeout = 100)
+    event, values = window.Read(timeout=100)
     if event is None or event in [sg.WIN_CLOSED]:
         os._exit(0)
     if event == '-SELECT-':
         with concurrent.futures.ThreadPoolExecutor() as executor:
             useful_key = UsefulKey(executor.submit(set_useful_key).result())
-        window["-SELECT-"].Update(text = f'{useful_key.key}')
-    if event == '-START-' and hasattr(useful_key, 'key'):
-        queue = Queue(maxsize = values['-SLIDER-']*3+10 if values['-SLIDER-'] < 4 else values['-SLIDER-']*3, n = int(values['-SLIDER-']))
+        window['-SELECT-'].Update(text=f'{useful_key.key}')
+    if event == '-START-' and 'useful_key' in globals():
+        queue = Queue(maxsize=values['-SLIDER-']*3+10 if values['-SLIDER-'] < 4 else values['-SLIDER-']*3, n=int(values['-SLIDER-']))
         if run == True:
             run = False
             queue.can_change = False
             thread.do_run = False
-            window["-START-"].Update(text = 'START')
+            window['-START-'].Update(text='START', button_color='green')
         else:
             run = True
             queue.can_change = True
-            window["-START-"].Update(text = 'STOP')
-            threading.Thread(target = register_keys).start()
-            thread = threading.Thread(target = useful_key.check_useful_key_pressed)
+            window['-START-'].Update(text='STOP', button_color='red')
+            threading.Thread(target=register_keys).start()
+            thread = threading.Thread(target=useful_key.check_useful_key_pressed)
             thread.do_run = True
             thread.start()
